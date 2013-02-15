6608573b2b7319d80c6cbb4cc1870b875ef0d2fb
CYBUTEK <admin@cybutek.net>
2013-02-15 02:16:11 +0000

    Version 0.6.0.1

--- Engineer/Version.cs
+++ Engineer/Version.cs
@@ -10,7 +10,7 @@ namespace Engineer
 {
     class Version
     {
-        public const string VERSION = "0.6";
+        public const string VERSION = "0.6.0.1";
         public const string PRODUCT_NAME = "engineer_redux";
         private string remoteVersion = null;
         private bool hasCompared = false;
